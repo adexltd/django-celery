ba681e1b7c84674e15fc84546b0475b6a6681e28
Ashok Pokharel <ashokpokharel977@gmail.com>
2023-01-04 14:44:52 +0545

    changed setting

--- django_celery/celery.py
+++ django_celery/celery.py
@@ -2,8 +2,9 @@ import os
 
 from celery import Celery
 from django.apps import apps
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery.settings")
 app = Celery("django_celery")
 app.config_from_object("django.conf:settings")
-app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
+app.autodiscover_tasks(settings.INSTALLED_APPS)
